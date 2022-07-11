831650535ad998cb4bbd498511af61a99a0c8b9d
VictorAldica <victor.aldica.official@gmail.com>
2022-07-12 02:07:17 +0300

    WIP Date class

---
+++ date/src/date.h
@@ -0,0 +1,21 @@
+#pragma once 
+#include <iostream>
+
+class Date
+{private:
+    int day_;
+    int month_;
+    int year_;
+    int hour_;
+    int minute_;
+
+public:
+    Date();
+    Date(int);
+    Date(int,int);
+    Date(int,int,int);
+    Date(int,int,int,int);
+    Date(int,int,int,int,int);
+    //~Date();
+    void FormatDate();
+};
