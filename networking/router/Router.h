d936a804acee05bdf7bf3b79315fb68e88773d64
Alex-dev02 <ionut02alex@yahoo.ro>
2022-07-11 11:38:35 +0300

    commented code, removed tests

--- networking/router/Router.h
+++ networking/router/Router.h
@@ -1,6 +1,12 @@
 #include <unordered_map>
 #include <functional>
 
+// The route handler should return a string, representing the data
+// returned by the handler
+// The input string should be either the payload for a POST function
+// Or the variables transmited through the path with GET/PUT
+// ex: /myPath/temp?new=15 -> std::string("new=15");
+
 class Router {
     static std::unordered_map<std::string, std::function<std::string(std::string)>> routes;
 public:
--- thermostat/src/main.cc
+++ thermostat/src/main.cc
@@ -3,13 +3,6 @@
 #include "thermostat.cc"
 #include "../../networking/router/Router.h"
 
-std::string f(std::string str) {
-    return str;
-}
-
 int main() {
-    Router r;
-    std::string path = "/r";
-    r.addPath(path, &f);
     std::cout << "Hello from Thermostat\n";
 }
