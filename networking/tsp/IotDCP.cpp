c4295f50f143ae37537f36cdb5f9a86f04089815
Alex-dev02 <ionut02alex@yahoo.ro>
2022-07-16 17:54:35 +0300

    IotDCP instead of http to talk between servers

---
+++ networking/tsp/IotDCP.cpp
@@ -0,0 +1,19 @@
+#include "IotDCP.hpp"
+
+IotDCP::IotDCP(std::string version):
+    m_version(version)
+{}
+
+std::string IotDCP::GetVersion() {
+    return m_version;
+}
+
+std::string IotDCP::CreateRequest(std::string type, std::string path) {
+    return type + ' ' + path + ' ' + "IotDCP/" + m_version;
+}
+
+std::string IotDCP::CreateResponse(std::string response_code, std::string content) {
+    return response_code + ' ' + "IotDCP/" + m_version + '\n' +
+        "Length " + std::to_string(content.length()) +
+        (content.length() == 0 ? "" : '\n' + content); 
+}
---
+++ networking/tsp/IotDCP.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+
+// IotDeviceCommunicationProtocol
+
+/*
+    ### Requests
+    - start with the name of the acction: GET or PUT
+        - GET is used for retrieving data from the server
+        - PUT is used for saving/modifing data on the server
+    - the path, which will represent the name of the function to execute
+        ex: /AddValve
+    - we can specify name of variables and their values by attaching them
+        to the path, like HTTP does: /AddValue?var=5
+    - the path must be followed by a white space
+    - the abreviation of the protocol's name and it's version
+        ex: IotDCP/0.1
+    ### Responses
+    - must start with a response code and the meaning of it:
+        - 0 Server, Error for faild to GET/PUT 
+        - 1 OK, for success
+        - 2 Not Found, for path not found
+        - 3 Not Authorized, for unauthorized
+    - the response code must be followed by a white spaces,
+        the abreviation of the protocol and its version: 1 IotDCP/0.1
+    - the response code and protocol name must be followed by a new line
+    - the next line must be specifing the length of the data sent back by the
+        server, if any: Length 0
+    - new line before the content, if any
+    
+    ex of full request:
+        GET /current_temp IotDCP/0.1
+    ex of full response:
+        1 OK IotDCP/0.1
+        Length 3
+        20C
+
+
+*/
+
+class IotDCP {
+public:
+    IotDCP(std::string version = "0.1");
+
+    std::string GetVersion();
+
+    std::string CreateRequest(
+        std::string type = "GET",
+        std::string path = "/"
+    );
+    std::string CreateResponse(
+        std::string response_code,
+        std::string content
+    );
+private:
+    std::string m_version = "0.1";
+};
