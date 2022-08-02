ae42b455cdcbd4eebd71802d81ddc1a4c521680c
Alex-dev02 <ionut02alex@yahoo.ro>
2022-08-02 13:35:22 +0300

    finished unit tests for Request

--- tests/RequestTest.cpp
+++ tests/RequestTest.cpp
@@ -3,6 +3,7 @@
 #include "../networking/communication/Request.hpp"
 
 TEST(RequestTests, GetPath) {
+    // IotDCP
     Request request("PUT /mypath/animals?animal=dog IotDCP/0.1");
     ASSERT_EQ(
         "/mypath/animals",
@@ -18,10 +19,25 @@ TEST(RequestTests, GetPath) {
         "/path/without/any/vars/test",
         request.GetPath()
     );
+
+    // HTTP
+
+    request = Request("GET /mypath/http HTTP/1.1\nContent-Length: 0\nContent-Type: text/plain\n\n");
+    ASSERT_EQ(
+        "/mypath/http",
+        request.GetPath()
+    );
+
+    request = Request("PUT /mypath/path?varname=varvalue HTTP/1.1\nContent-Length: 0\nContent-Type: text/plain\n\n");
+    ASSERT_EQ(
+        "/mypath/path",
+        request.GetPath()
+    );
 }
 
 TEST(RequestTests, GetPathVar) {
-    Request request("PUT /mypath/animal?animal=cat&name=john IotDCP/0.1");
+    // IotDCP
+    Request request("PUT /mypath/animal?animal=cat&name=john&age=3 IotDCP/0.1");
     ASSERT_EQ(
         "cat",
         request.GetPathVar("animal")
@@ -29,5 +45,34 @@ TEST(RequestTests, GetPathVar) {
     ASSERT_EQ(
         "john",
         request.GetPathVar("name")
+    );
+    ASSERT_EQ(
+        "3",
+        request.GetPathVar("age")
+    );
+
+    request =
+        Request("PUT /mypath/path?insect=fly&age=1 HTTP/1.1\nContent-Length: 0\nContent-Type: text/plain\n\n");
+    ASSERT_EQ(
+        "fly",
+        request.GetPathVar("insect")
+    );
+    ASSERT_EQ(
+        "1",
+        request.GetPathVar("age")
+    );
+}
+
+TEST(RequestTest, GetRawRequest) {
+    Request request("GET /path/to/source IotDCP/0.1");
+    ASSERT_EQ(
+        "GET /path/to/source IotDCP/0.1",
+        request.GetRawRequest()
+    );
+    
+    request = Request("PUT /home/page.php HTTP/1.1\nContent-Length: 0\nContent-Type: text/plain\n\n");
+    ASSERT_EQ(
+        "PUT /home/page.php HTTP/1.1\nContent-Length: 0\nContent-Type: text/plain\n\n",
+        request.GetRawRequest()
     );
 }
