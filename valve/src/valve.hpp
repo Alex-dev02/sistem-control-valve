a05413ea3c9dac533c5ef8b839f22d006fa46b87
Dorin Lazăr <dorin.lazar.ext@siemens.com>
2022-08-05 14:33:37 +0300

    stuff

--- valve/src/valve.hpp
+++ valve/src/valve.hpp
@@ -1,29 +1,28 @@
 #pragma once
 
-#include <router.hpp>
-#include <iot_dcp.hpp>
-#include <request.hpp>
-#include <response.hpp>
-
-
+#include <networking/iot_dcp.hpp>
+#include <networking/request.hpp>
+#include <networking/response.hpp>
+#include <networking/router.hpp>
 #include <string>
 
 class Valve {
-public:
-    Valve();
+ public:
+  Valve();
 
-    Router GetRouter();
-    float GetCurrentTarget();
-    float GetTemperature();
+  Router GetRouter();
+  float GetCurrentTarget();
+  float GetTemperature();
 
-    void SetCurrentTarget(float current_targer);
-    void SetTemperature(float temperature);
-    void IncrementTemperature();
-private:
-    float m_current_target;
-    float m_temperature;
+  void SetCurrentTarget(float current_targer);
+  void SetTemperature(float temperature);
+  void IncrementTemperature();
 
-    Router m_router;
+ private:
+  float m_current_target;
+  float m_temperature;
 
-    Response SetCurrentTargetRoute(Request request);
+  Router m_router;
+
+  Response SetCurrentTargetRoute(Request request);
 };
